dd709112b484d9cbeb2792fe9223635fdc6b95b2
Ibai Fernández <info@ibaifernandez.com>
2023-02-15 14:29:03 -0500

    Wrapping up exercise

--- src/models.py
+++ src/models.py
@@ -1,30 +1,52 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), ForeignKey('user.id'))
+    character_name = Column(String(250))
+    planet_name = Column(String(250))
+    vehicle_name = Column(String(250))
+    person = relationship(User) # ???
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favorites.character_name'))
+    mass = Column(Integer)
+    height = Column(String(250))
+    favorites = relationship(Favorites) # ???
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favorites.planet_name'))
+    population = Column(Integer)
+    terrain = Column(String(250))
+    # person = relationship(User) # ???
+
+class Vechicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favorites.planet_name'))
+    speed = Column(Integer)
+    manufacturer = Column(String(250))
+    # person = relationship(User) # ???
 
     def to_dict(self):
         return {}
